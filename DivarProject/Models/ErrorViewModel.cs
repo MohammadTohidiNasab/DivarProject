7273410b0e3ec680f618be6c5772512b04e889a8
Mohammad Tohidi <mohammadtohidi.mtn@gmail.com>
2024-09-21 21:24:46 -0800

    Add project files.

---
+++ DivarProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DivarProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
